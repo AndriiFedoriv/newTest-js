d5e37289ea98a8d5c251ad53f845243eaa32dff5
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2020-03-09 20:59:06 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript?fbclid=IwAR2kovrpho_c71HBlvmwh0Tj3_K7JYTrCMYXzoulE-Qs3e6Sg_8Mxfol9iQ
+
+function DNAtoRNA(dna) {
+  return dna.split('T').join('U');
+}
+
