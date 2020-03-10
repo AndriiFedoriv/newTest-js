cf9f69d039116e3b194855c6815218122ac8ec8d
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2020-03-10 14:19:58 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,14 @@ function DNAtoRNA(dna) {
   return dna.split('T').join('U');
 }
 
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript?fbclid=IwAR1Qj_v5YUbJWWrWNKaxwZ30glNLMPwlOETGIX6J5SsdhR77W4sH1crN5ns
+
+var min = function(list) {
+  list[0] = Math.min(...list);
+  return list[0];
+};
+
+var max = function(list) {
+  list[0] = Math.max(...list);
+  return list[0];
+};
